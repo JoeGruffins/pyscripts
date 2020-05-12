ae80332507a5e87db02576a93fbe67c6bf8d5762
JoeGruff <joegruffins@gmail.com>
2020-05-12 20:56:29 +0900

    Add aplphabetize.

--- ecpair/main.py
+++ ecpair/main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from decred.crypto import crypto
 from decred.dcr import addrlib
-from decred.dcr.nets import mainnet
+from decred.dcr.nets import mainnet, testnet, simnet
 from decred.util.encode import ByteArray
+from base58 import b58decode, b58encode
 
 
 def main():
@@ -12,6 +13,14 @@ def main():
     pkh = crypto.hash160(pub.serializeCompressed().b)
     addrPKH = addrlib.AddressPubKeyHash(pkh, mainnet)
     print([x for x in ba])
+    print(pub.serializeCompressed().b.hex())
+    print(addrPKH.string())
+    h = ByteArray("751e76e8199196d454941c45d1b3a323f1433bd6")
+    addrPKH = addrlib.AddressPubKeyHash(h, mainnet)
+    print(addrPKH.string())
+    addrPKH = addrlib.AddressPubKeyHash(h, testnet)
+    print(addrPKH.string())
+    addrPKH = addrlib.AddressPubKeyHash(h, simnet)
     print(addrPKH.string())
 
 
